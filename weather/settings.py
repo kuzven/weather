7655593c8b39fa84d0b37fa796d2360a5854f973
kuzven <kuzven@gmail.com>
2025-05-29 13:22:44 -0700

    Added SESSION_ENGINE in settings.py

--- weather/settings.py
+++ weather/settings.py
@@ -148,3 +148,5 @@ else:
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SESSION_ENGINE = "django.contrib.sessions.backends.db"
